1646e6f5e9ec4559e0502e06804e20bac9c321b7
Kotaro-Axel <axel_uxu@hotmail.com>
2020-12-08 13:21:57 -0600

    created API Products and Gmail Auth

--- TrayectoriaBack/urls.py
+++ TrayectoriaBack/urls.py
@@ -1,18 +1,3 @@
-"""TrayectoriaBack URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, re_path
 from django.conf.urls import include
@@ -23,6 +8,9 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.authtoken import views
+
+from api import views
+
 
 
 
@@ -39,13 +27,19 @@ class UserViewSet(viewsets.ModelViewSet):
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register('products', views.ProductView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^', include(router.urls)),
-    re_path(r'^api/v1/login/', include('Login.urls')),
-    re_path(r'^api/v1/profile/', include('Profile.urls')),
-    re_path(r'^api/v1/user/', include('User.urls')),
-    #path('api_generate_token/',views.obtain_auth_token),
+    #API_URLS
+    path('api/v1/', include(router.urls)),
+    #AUTH_URLS
+    path('api/v1/auth/', include('rest_auth.urls')),
+    path('api/v1/auth/registration/', include('rest_auth.registration.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
+# re_path(r'^', include(router.urls)),
+# re_path(r'^api/v1/login/', include('Login.urls')),
+# re_path(r'^api/v1/profile/', include('Profile.urls')),
+# re_path(r'^api/v1/user/', include('User.urls')),
+#path('api_generate_token/',views.obtain_auth_token),
